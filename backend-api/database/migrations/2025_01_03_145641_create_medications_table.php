ec7b9b0cc8fa19a5e99864a4cacf430609eda21c
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2025-01-03 18:04:15 +0300

    Update 2025_01_03_145641_create_medications_table.php

--- backend-api/database/migrations/2025_01_03_145641_create_medications_table.php
+++ backend-api/database/migrations/2025_01_03_145641_create_medications_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('medications', function (Blueprint $table) {
             $table->id();
+
             $table->timestamps();
         });
     }
