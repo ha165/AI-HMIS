b3cf91989fc4d8c83738dbf47a32d354a9719b6a
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2025-01-03 18:01:07 +0300

    Initial commit

---
+++ backend-api/app/Models/Medications.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Medications extends Model
+{
+    /** @use HasFactory<\Database\Factories\MedicationsFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'medication_name',
+        'dosage',
+        'side_effects',
+        'manufacturer',
+    ];
+}
