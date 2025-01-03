db6a0d080d709600b0e290a0108280407cee6ee9
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2025-01-03 18:56:55 +0300

    Update api.php

--- backend-api/routes/api.php
+++ backend-api/routes/api.php
@@ -1,12 +1,10 @@
 <?php
 
+use App\Http\Controllers\AppointmentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 //Route::get('/user', function (Request $request) {
 //return $request->user();
-//})->middleware('auth:sanctum');
-Route::apiResource("appointments",App\Http\Controllers\AppointmentController::class)->middleware("auth:sanctum","");
-Route::get("/", function (Request $request) {
-    return "API";
-});
+//})->middleware('auth:sanctum');::class)->middleware("auth:sanctum","");
+Route::apiResource("appointments", AppointmentController::class);
