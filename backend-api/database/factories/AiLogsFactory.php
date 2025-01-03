089d8d66c2297d774f19d364b29134a8e32ec68b
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2025-01-03 18:32:58 +0300

    AI logs

---
+++ backend-api/database/factories/AiLogsFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Ai_logs>
+ */
+class AiLogsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
