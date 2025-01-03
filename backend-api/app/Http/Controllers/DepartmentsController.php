694d2fe64342bdfa20ad6932a9259345de2283bd
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2025-01-03 18:33:49 +0300

    Departments APi

---
+++ backend-api/app/Http/Controllers/DepartmentsController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Departments;
+use App\Http\Requests\StoreDepartmentsRequest;
+use App\Http\Requests\UpdateDepartmentsRequest;
+
+class DepartmentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDepartmentsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Departments $departments)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDepartmentsRequest $request, Departments $departments)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Departments $departments)
+    {
+        //
+    }
+}
