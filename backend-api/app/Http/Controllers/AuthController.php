f694d4b25b074a7fa6ae50cbea3c04e5240515bc
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2025-01-07 18:12:13 +0300

    Update AuthController.php

--- backend-api/app/Http/Controllers/AuthController.php
+++ backend-api/app/Http/Controllers/AuthController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
    public function register(request $request) {
-     return "register"; 
+     $fields = $request->validate([
+       'name' => 'required|max:255',
+       'email' => 'required|email|unique:users',
+       'password' => 'required|min:8|confirmed'
+     ]);
+     User::created($fields);
    }
    public function login(request $request) {
      return "login";
