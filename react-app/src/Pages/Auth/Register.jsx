d1cf3e3026c8a9728f6d9fe7f0e1b7d0dcd8d624
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2025-01-10 15:27:39 +0300

    updates

--- react-app/src/Pages/Auth/Register.jsx
+++ react-app/src/Pages/Auth/Register.jsx
@@ -1,8 +1,10 @@
 import { useNavigate} from "react-router-dom";
-import { useState } from "react";
+import { useState, useContext} from "react";
 import {ToastContainer,toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { AppContext } from "../Context/AppContext";
 export default function Register() {
+  const {setToken } = useContext(AppContext)
   const navigate = useNavigate();
   const [formdata, setFormData] = useState({
     name: "",
--- react-app/src/Pages/Context/AppContext.jsx
+++ react-app/src/Pages/Context/AppContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState} from "react";
 export const AppContext = createContext();
 
 export default function AppProvider(children) {
-   const [token, setToken] = useState("localstorage.getItem('token')");
+    const [token, setToken] = useState(localStorage.getItem("token"));
     return (
         <AppContext.Provider value={{token,setToken}}>
             {children}
