2d55e536a3101472bdeae3e91be8c59dd1d973dc
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2025-01-10 12:08:30 +0300

    updates

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Pages/Layout";
+import Home from "./Pages/Home";
 import "./App.css";
 export default function App() {
   return (
