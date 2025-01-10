760a60da687d2859bcc9838d49b8a2b5cb405918
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2025-01-10 11:20:32 +0300

    Update api.php

--- backend-api/routes/api.php
+++ backend-api/routes/api.php
@@ -5,10 +5,10 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
-//Route::get('/user', function (Request $request) {
-//return $request->user();
-//})->middleware('auth:sanctum');::class)->middleware("auth:sanctum","");
 Route::apiResource("appointments", AppointmentController::class);
 Route::apiResource("posts", PostController::class);
 Route::post("/register", [AuthController::class, "register"]);
