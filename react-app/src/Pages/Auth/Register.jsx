3e55255cc1bffc8dea4aeb6cf6f62162ce949d21
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2025-01-10 13:02:02 +0300

    Update Register.jsx

--- react-app/src/Pages/Auth/Register.jsx
+++ react-app/src/Pages/Auth/Register.jsx
@@ -1,7 +1,52 @@
+import { useState } from "react";
 export default function Register() {
-    return (
+  const [formdata, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  async function handleRegister(e) {
+    e.preventDefault();
+    const res = await fetch("api/register", {
+        method: "POST",
+        body: JSON.stringify(formdata),
+    });
+
+    const data = await res.json();
+
+    if (data.errors) {
+        setErrors(data.errors);
+    }else {
+        console.log(data);
+    }
+  }
+
+  return (
+    <div>
+      <h1 className="title">Register</h1>
+      <form onSubmit={handleRegister} className="w-1/2 mx-auto space-y-6">
         <div>
-            <h1 className="title">Register</h1>
+          <input type="text" placeholder="name" value={formdata.name} onChange={(e) => setFormData({ ...formdata, name: e.target.value })}/>
+          {errors.name && <span className="error">{errors.name[0]}</span>}
         </div>
-    );
+        <div>
+          <input type="text" placeholder="Email" value={formdata.email} onChange={(e) => setFormData({ ...formdata, email: e.target.value })} />
+          {errors.email && <span className="error">{errors.email[0]}</span>}
+        </div>
+        <div>
+          <input type="Password" placeholder="Password" value={formdata.password} onChange={(e) => setFormData({ ...formdata, password: e.target.value })} />
+          {errors.password && <span className="error">{errors.password[0]}</span>}
+        </div>
+        <div>
+          <input type="Password" placeholder="Confirm Password"  value={formdata.password_confirmation} onChange={(e) => setFormData({ ...formdata, password_confirmation: e.target.value })}/>
+        </div>
+
+        <button className="primary-btn">Register</button>
+      </form>
+    </div>
+  );
 }
