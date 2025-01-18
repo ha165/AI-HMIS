a041a30e6bdfebf72281b0270e5c337c988e2d66
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2025-01-18 15:35:59 +0300

    Update App.jsx

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,3 +1,6 @@
+import { ColorModeContext, useMode } from "../themes.js";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import  TopBar  from "./Scenes/global/TopBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Pages/Layout";
 import Home from "./Pages/Home";
@@ -7,18 +10,31 @@ import Login from "./Pages/Auth/Login";
 import { useContext } from "react";
 import { AppContext } from "./Context/AppContext";
 export default function App() {
-  const {user} = useContext(AppContext);
+  const [theme, colorMode] = useMode();
+  const { user } = useContext(AppContext);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <TopBar />
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Layout />}>
+                  <Route index element={<Home />} />
 
-          
-          <Route path ="/register" element={user ? <Home /> : <Register />} />
-          <Route path ="/login" element={user ? <Home /> : <Login />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+                  <Route
+                    path="/register"
+                    element={user ? <Home /> : <Register />}
+                  />
+                  <Route path="/login" element={user ? <Home /> : <Login />} />
+                </Route>
+              </Routes>
+            </BrowserRouter>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
