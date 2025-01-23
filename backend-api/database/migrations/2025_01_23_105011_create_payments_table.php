e5037c5c3f03c9e1bde8c1d76e97d503d7bcf83a
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2025-01-23 13:57:11 +0300

    updates

---
+++ backend-api/database/migrations/2025_01_23_105011_create_payments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('bill_id')->constrained('bills')->cascadeOnDelete();
+            $table->foreignId('patient_id')->constrained('patients')->cascadeOnDelete();
+            $table->string('payment_method');
+            $table->decimal('amount', 10, 2);
+            $table->string('transaction_id')->nullable();
+            $table->date('payment_date')->default(now());
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
