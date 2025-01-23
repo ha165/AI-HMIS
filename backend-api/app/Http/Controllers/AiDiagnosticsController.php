5fcd97e6a61bc3f01bd70ee6313f7f011180bf42
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2025-01-23 14:00:56 +0300

    updates

---
+++ backend-api/app/Http/Controllers/AiDiagnosticsController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ai_diagnostics;
+use App\Http\Requests\StoreAi_diagnosticsRequest;
+use App\Http\Requests\UpdateAi_diagnosticsRequest;
+
+class AiDiagnosticsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAi_diagnosticsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Ai_diagnostics $ai_diagnostics)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAi_diagnosticsRequest $request, Ai_diagnostics $ai_diagnostics)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Ai_diagnostics $ai_diagnostics)
+    {
+        //
+    }
+}
