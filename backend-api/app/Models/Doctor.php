337920175b6f7475f0e2a54fea5457bea9fc3fcb
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2025-03-01 13:13:24 +0300

    Update Doctor.php

--- backend-api/app/Models/Doctor.php
+++ backend-api/app/Models/Doctor.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Doctor extends Model
 {
     use HasFactory;
-    protected $table = 'doctors';
+    protected $table = 'Doctors';
 
     protected $fillable = [
         'user_id',
