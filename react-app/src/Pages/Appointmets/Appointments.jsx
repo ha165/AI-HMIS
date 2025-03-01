61a15c6d8698367d5ca7ca37a550146c37de0f5b
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2025-03-01 12:06:23 +0300

    Update Appointments.jsx

--- react-app/src/Pages/Appointmets/Appointments.jsx
+++ react-app/src/Pages/Appointmets/Appointments.jsx
@@ -131,13 +131,12 @@ const Appointments = () => {
 
   const columns = [
     { field: "id", headerName: "ID", width: 90 },
-    { field: "first_name", headerName: "First Name", flex: 1 },
-    { field: "last_name", headerName: "Last Name", flex: 1 },
-    { field: "email", headerName: "Email", flex: 1 },
+    { field: "first_name", headerName: "Patient Name", flex: 1 },
+    { field: "doctor_name", headerName: "Doctor Name", flex: 1 },
     { field: "phone", headerName: "Phone Number", flex: 1 },
-    { field: "address", headerName: "Address", flex: 1 },
     { field: "specialization", headerName: "Specialization", flex: 1 },
-    { field: "license_number", headerName: "License Number", flex: 1 },
+    { field: "appointment_date", headerName: "Date", flex: 1 },
+    { field: "status", headerName: "Status", flex: 1 },
     {
       field: "actions",
       headerName: "Actions",
