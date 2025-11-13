5ed974ef29b5e3a05ebeb2e35377e7f172148e54
Harmony Lumumba Muyagane <66202605+ha165@users.noreply.github.com>
2025-11-13 14:03:14 +0300

    Update Medical_records.php

--- backend-api/app/Models/Medical_records.php
+++ backend-api/app/Models/Medical_records.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Medical_Records extends Model
+class Medical_records extends Model
 {
     use HasFactory, SoftDeletes;
     protected $table = 'medical_records';
