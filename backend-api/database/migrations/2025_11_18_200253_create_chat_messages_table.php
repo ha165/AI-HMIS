8173ed8c873b07d226d5df8f39993f3f97088bef
Harmony Lumumba Muyagane <66202605+ha165@users.noreply.github.com>
2025-11-18 23:07:18 +0300

    create chatmessages model

--- backend-api/database/migrations/2025_11_18_200253_create_chat_messages_table.php
+++ backend-api/database/migrations/2025_11_18_200253_create_chat_messages_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('chat_messages', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->enum('role', ['user', 'assistant']);
+            $table->text('content');
             $table->timestamps();
         });
     }
